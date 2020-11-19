74e85c440539102c2d4e542dd41547f37452f8f2
jaswinder <jaswinderjk.07@gmail.com>
2020-11-19 09:34:01 +0530

    initial code setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import React from 'react';
+
+class App extends React.Component
+{
+  render(){
+    return(
+      <div>
+        <h1>Helllo</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
